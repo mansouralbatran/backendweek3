6127231d6097cf52f7d21acc8840b7dc2bf75abe
mansouralbatran <mansouralbatran@gmail.com>
2021-08-25 02:15:09 +0300

    finishing lab13 backend

--- server.js
+++ server.js
@@ -30,6 +30,8 @@ server.get('/test', testHandler);
 
 server.post('/addbook',AddBook)
 
+server.delete('/deleteCat/:BookId',DeleteBook)
+
 server.listen(PORT, () => {
     console.log(`Listning on PORT ${PORT}`)
 })
@@ -65,7 +67,7 @@ function seedDataCollection() {
 
  function getbookfunction(req,resp){
     let email1 = req.query.useremail;
-    console.log(email1);
+    // console.log(email1);
     bookModel.find({email:email1},function(err,ownerData){
         if(err) {
             console.log('error in getting the data')
@@ -79,7 +81,7 @@ function seedDataCollection() {
  }
  async function AddBook(req,res){
      let bookTitle=req.body
-     console.log(bookTitle);
+    //  console.log(bookTitle);
      const addedBook = new bookModel({
         title:bookTitle.Title1,
         description:bookTitle.Description1,
@@ -93,14 +95,35 @@ function seedDataCollection() {
         if (err) {
             console.log('error in getting the data')
         } else {
-            console.log(ownerData);
+            // console.log(ownerData);
             res.send(ownerData)
         }
     })
-    
-
 
  }
+//  BookId
+function DeleteBook(req,res){
+let Email=req.query.email
+let DeletedBook=req.params.BookId
+console.log(Email);
+bookModel.remove({_id:DeletedBook},function(error,DeletedData){
+    if(error){
+        console.log('Error in deleting the Item');
+    }
+    else{
+        console.log('Data deleted',DeletedData);
+        bookModel.find({email:Email}, function (err, RemainedData) {
+            if (err) {
+                console.log('error in getting the data')
+            } else {
+                // console.log(DeletedData);
+                res.send(RemainedData)
+
+            }
+        })
+    }
+})
+}
 
 
 
